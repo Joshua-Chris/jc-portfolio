5bf5fe53f3397c306a05609c00201045493e89df
Joshua-Chris <42803479+Joshua-Chris@users.noreply.github.com>
2023-08-18 15:35:47 +0100

    Update Contact.jsx
    Added top-padding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-gray-900 flex justify-center items-center p-4 pb-0'>
+    <div name='contact' className='w-full h-screen bg-gray-900 flex justify-center items-center p-4 pt-8 pb-0'>
         <form method='POST' action="https://getform.io/f/e9a2c4a7-5310-4746-886b-b2bff36c75e4" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300'>Contact</p>
