6793082761a5320cb8c9ce8c08d4d8a3e67f76ad
Joshua-Chris <42803479+Joshua-Chris@users.noreply.github.com>
2023-08-18 15:26:11 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,9 +10,9 @@ const Work = () => {
   return (
     <div
       name="projects"
-      className="w-full md:h-screen text-gray-300 bg-gray-900 my-4"
+      className="w-full md:h-screen text-gray-300 bg-gray-900"
     >
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 pt-8 pb-8 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-blue-600">
             Projects
