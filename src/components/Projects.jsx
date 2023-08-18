ebf98497a47324df824084b11a890dc7a29398d2
Joshua-Chris <42803479+Joshua-Chris@users.noreply.github.com>
2023-08-18 15:21:24 +0100

    Update Projects.jsx
    Added my-4 to the projects-section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,7 @@ const Work = () => {
       name="projects"
       className="w-full md:h-screen text-gray-300 bg-gray-900"
     >
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 my-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-blue-600">
             Projects
