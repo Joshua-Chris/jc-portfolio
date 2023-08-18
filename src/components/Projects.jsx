0ccfd70ccb9e7a148ad008f07c2634e8831e1778
Joshua-Chris <42803479+Joshua-Chris@users.noreply.github.com>
2023-08-18 15:54:25 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,8 +13,8 @@ const Work = () => {
       className="w-full md:h-screen text-gray-300 bg-gray-900"
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="py-8">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-blue-600">
+        <div className="pb-8">
+          <p className="pt-8 text-4xl font-bold inline border-b-4 text-gray-300 border-blue-600">
             Projects
           </p>
           <p className="py-6">A list of some of my projects</p>
