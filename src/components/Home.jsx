548d2cedf710c9a5367965d0676d0785f4a54801
Joshua-Chris <42803479+Joshua-Chris@users.noreply.github.com>
2024-05-16 13:20:00 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
           <p className='text-blue-600'>Hi, my name is</p>
           <h1 className='text-5xl sm:text-7xl font-bold text-[#ccd6f6]'>Joshua Chris</h1>
-          <h2 className='text-4xl sm:text-5xl font-bold text-[#8892b0]'>I'm a Frontend Developer</h2>
+          <h2 className='text-4xl sm:text-5xl font-bold text-[#8892b0]'>I'm a Frontend Engineer</h2>
           <p className='text-[#8892b0] py-4 max-w-700px'>I specialize in building exceptionally outstanding client side 
             interfaces and experiences, using JavaScript and ReactJs
           </p>
